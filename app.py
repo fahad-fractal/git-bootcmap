41881a0e190c6c9f05c6e0effed3d86caf7fb911
fahad ansari <fahad.ansari@fractal.ai>
2021-11-02 13:53:53 +0530

    prod commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return 'Web App with Python Flask in production!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
